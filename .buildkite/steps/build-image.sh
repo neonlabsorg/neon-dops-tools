54df3b94824471d0e5f8bbe6eaf809922e0c46ee
rozhkovdmitrii <rozhkovdmitrii@yandex.ru>
2022-08-25 18:04:19 +0500

    Fix docker build

--- .buildkite/steps/build-image.sh
+++ .buildkite/steps/build-image.sh
@@ -3,5 +3,3 @@ source .buildkite/steps/utils.sh
 export REVISION=${BUILDKITE_TAG:-${BUILDKITE_COMMIT}}
 docker build . -t neonlabsorg/dops-tools:${REVISION}
 handle_error "Failed to build neonlabsorg/dops-tools:${REVISION}"
-
-docker images
